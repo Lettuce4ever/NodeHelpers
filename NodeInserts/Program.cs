770e0a88c9908c7bc16d57815e98b0d108eae9ad
User <User@LAB3-3>
2024-01-18 14:42:05 +0200

    jhkjh

--- NodeInserts/Program.cs
+++ NodeInserts/Program.cs
@@ -5,6 +5,22 @@ namespace NodeInserts
 {
     internal class Program
     {
+        static public bool AreThereRecivers(Node<ShabatRecievers> lst, int day, int month, int year)
+        {
+            while (lst != null)
+            {
+                if (lst.GetValue().GetDay()==day&& lst.GetValue().GetMonth()== month&& lst.GetValue().GetYear()== year)
+                {
+                    return true;
+                }
+                lst=lst.GetNext();
+            }
+            return false;
+        }
+        static public Node<ShabatRecievers> DeleteShabatReciever(Node<ShabatRecievers> lst, string name)
+        {
+            return NodeHelper.Delete(, name);
+        }
         static void Main(string[] args)
         {
             Node<ShabatRecievers> shabatShalom = new Node<ShabatRecievers>(new ShabatRecievers("Shiri","Shira",18,1,2024));
