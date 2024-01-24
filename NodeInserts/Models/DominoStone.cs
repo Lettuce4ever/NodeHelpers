3c9b64e311dce954f9d330bdc858f0bd10721c1d
User <User@LAB3-3>
2024-01-24 11:24:50 +0200

    Domino Started

---
+++ NodeInserts/Models/DominoStone.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeInserts.Models
+{
+    internal class DominoStone
+    {
+        private int num1;
+        private int num2;
+
+        public DominoStone(int num1, int num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+
+        public int GetNum1() { return num1; }
+        public int GetNum2() { return num2; }
+        public int SetNum1() { return num1; }
+        public int SetNum2() { return num2; }
+
+        public bool isExist(int num)
+        {
+            if (num==num1||)
+        }
+    }
+}
